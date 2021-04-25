001489c3754a0a46eeedf049c4edea0e1e06f1bc
csgrandeur <csgrandeur@gmail.com>
2021-04-25 17:50:05 +0800

    update code

---
+++ solutions/DataStructure/BinaryTree/Huffman/POJ3253.cpp
@@ -0,0 +1,40 @@
+// difficulty: 2
+// Fence Repair
+// 为赫夫曼树而出的题，还是赫夫曼树求权重和（最短编码长度）
+// 坑点1：与编码不同，如果只有一根模板而且不需要砍的话，返回0
+// 坑点2：直观看数据范围的积不大于 int，却容易忽视最终结果很多数会重复加，依然需要 long long
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<queue>
+using namespace std;
+typedef long long LL;
+const int maxn = 21000;
+const int maxl = 51000;
+int n, cnt[maxn];
+
+LL GetHuffmanLength()
+{
+    LL length = 0;
+    priority_queue<LL, vector<LL>, greater<LL> > q;
+    for(int i = 0; i < n; i ++)
+        q.push(cnt[i]);
+    for(length = 0; q.size() > 1; )
+    {
+        int left = q.top(); q.pop();
+        int right = q.top(); q.pop();
+        length += left + right;
+        q.push(left + right);
+    }
+    return length;
+}
+int main()
+{
+    while(scanf("%d", &n) != EOF)
+    {
+        for(int i = 0; i < n; i ++)
+            scanf("%d", &cnt[i]);
+        printf("%lld\n", GetHuffmanLength());
+    }
+    return 0;
+}
