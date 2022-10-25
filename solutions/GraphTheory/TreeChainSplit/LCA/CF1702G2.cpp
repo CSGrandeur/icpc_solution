edcd5a9987a1d70cab9c142ca9467eee892d341c
csgrandeur <csgrandeur@gmail.com>
2022-10-26 01:07:39 +0800

    update

---
+++ solutions/GraphTheory/TreeChainSplit/LCA/CF1702G2.cpp
@@ -0,0 +1,139 @@
+// difficulty: 3
+// Passable Paths (hard version)
+// 按层排序，最深的节点必为链的一端
+// 枚举其它点，根据LCA的关系分类讨论更新另一端点
+// 出现矛盾则不是单链
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<queue>
+#include<algorithm>
+
+struct FSGraph
+{
+    std::vector<int> fst, nex, u, v, w;
+    void Init(int n_=-1)
+    {
+        nex.clear();
+        u.clear(); v.clear(); w.clear();
+        if(n_ >= 0) fst.resize(n_ + 10), std::fill(fst.begin(), fst.end(), -1);
+    }
+    void AddEdge(int u_, int v_, int w_=1)
+    {
+        nex.push_back(fst[u_]);
+        u.push_back(u_);
+        v.push_back(v_);
+        w.push_back(w_);
+        fst[u_] = nex.size() - 1;
+    }
+    void DbEdge(int u_, int v_, int w_=1, int w2_=-0x4f4f4f4f)
+    {
+        if(w2_ == -0x4f4f4f4f) w2_ = w_;
+        AddEdge(u_, v_, w_);
+        AddEdge(v_, u_, w2_);
+    }
+    std::vector<int> son, siz, dep, fa, top, dfn, rnk;
+    int rt, dcnt;
+    void TreeChainBuild(int rt_);
+    void TreeChainDFS1(int now);
+    void TreeChainDFS2(int now, int t);
+    std::vector<int> vw;
+    int LCA(int a, int b);
+};
+
+void FSGraph::TreeChainBuild(int rt_)
+{
+    rt = rt_;
+    dcnt = 1;
+    int n_ = fst.size() + 10;
+    son.resize(n_);
+    siz.resize(n_);
+    dep.resize(n_);
+    fa.resize(n_);
+    top.resize(n_);
+    dfn.resize(n_);
+    rnk.resize(n_);
+    dep[rt] = 1;
+    fa[rt] = 0;
+    top[0] = 0;
+    TreeChainDFS1(rt);
+    TreeChainDFS2(rt, rt);
+}
+void FSGraph::TreeChainDFS1(int now)
+{
+    son[now] = -1;
+    siz[now] = 1;
+    for(int i = fst[now]; i != -1; i = nex[i])
+    {
+        if(v[i] == fa[now]) continue;
+        fa[v[i]] = now;
+        dep[v[i]] = dep[now] + 1;
+        TreeChainDFS1(v[i]);
+        siz[now] += siz[v[i]];
+        if(son[now] == -1 || siz[v[i]] > siz[son[now]])
+            son[now] = v[i];
+    }
+}
+void FSGraph::TreeChainDFS2(int now, int t)
+{
+    top[now] = t;
+    dfn[now] = dcnt;
+    rnk[dcnt] = now;
+    dcnt ++;
+    if(son[now] == -1) return;
+    TreeChainDFS2(son[now], t);
+    for(int i = fst[now]; i != -1; i = nex[i])
+        if(v[i] != son[now] && v[i] != fa[now])
+            TreeChainDFS2(v[i], v[i]);
+
+}
+int FSGraph::LCA(int a, int b)
+{
+    while(top[a] != top[b])
+    {
+        if(dep[top[a]] >= dep[top[b]]) a = fa[top[a]];
+        else b = fa[top[b]];
+    }
+    return dep[a] < dep[b] ? a : b;
+}
+const int maxn = 2e5 + 10;
+int n, q, k, a, b, p[maxn];
+FSGraph fsg;
+bool cmp(const int &a, const int &b)
+{
+    return fsg.dep[a] > fsg.dep[b];
+}
+bool Judge()
+{
+    if(k <= 2) return true;
+    std::sort(p, p + k, cmp);
+    int l = 0, r = 1, lcap = fsg.LCA(p[l], p[r]);
+    for(int i = 2; i < k; i ++)
+    {
+        if(p[i] == lcap) continue;
+        int lcal = fsg.LCA(p[l], p[i]), lcar = fsg.LCA(p[r], p[i]);
+        if(lcal == p[i] && lcar == lcap || lcar == p[i] && lcal == lcap) continue;
+        else if(lcal == lcar && p[r] == lcap) lcap = lcal, r = i;
+        else return false;
+    }
+    return true;
+}
+int main()
+{
+    while(scanf("%d", &n) != EOF)
+    {
+        fsg.Init(n);
+        for(int i = 1; i < n; i ++)
+            scanf("%d%d", &a, &b), fsg.DbEdge(a, b);
+        fsg.TreeChainBuild(n >> 1);
+        int t = 0;
+        for(t = 0, scanf("%d", &q); t < q; t ++)
+        {
+            scanf("%d", &k);
+            for(int i = 0; i < k; i ++)
+                scanf("%d", &p[i]);
+            printf(Judge() ? "YES\n" : "NO\n");
+        }
+    }
+    return 0;
+}
